c18964a0fb298d96a229a8ecefec639736fe5e79
DESKTOP-ORRN0CM\Toan Dang <toandang2309@gmail.com>
2023-01-12 16:34:31 +0700

    Update Components ProductItem

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -2,65 +2,69 @@ import { Grid, Card, Col, Text, Row, Button } from "@nextui-org/react";
 
 const ProductItem = (props) => {
     const { productData } = props;
-    return (
-        <Grid xs={3}>
-            <Card css={{ w: "100%", h: "400px" }}>
-                <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-                    <Col>
-                        <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
-                            New
-                        </Text>
-                        <Text h3 color="black">
-                            Acme camera
-                        </Text>
-                    </Col>
-                </Card.Header>
-                <Card.Body css={{ p: 0 }}>
-                    <Card.Image
-                        src={productData.image}
-                        width="100%"
-                        height="100%"
-                        objectFit="cover"
-                        alt="Card example background"
-                    />
-                </Card.Body>
-                <Card.Footer
-                    isBlurred
-                    css={{
-                        position: "absolute",
-                        bgBlur: "#ffffff66",
-                        borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
-                        bottom: 0,
-                        zIndex: 1,
-                    }}
-                >
-                    <Row>
+    let availableProduct = productData.available;
+    console.log(availableProduct);
+    if ({ availableProduct } === false) {
+        return (
+            <Grid xs={3}>
+                <Card css={{ w: "100%", h: "400px" }}>
+                    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
                         <Col>
-                            <Text color="#000" size={14}>
-                                {productData.name}
+                            <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+                                New
                             </Text>
-                            <Text color="#000" size={14}>
-                                {productData.price}
+                            <Text h3 color="black">
+                                Acme camera
                             </Text>
                         </Col>
-                        <Col>
-                            <Row justify="flex-end">
-                                <Button flat auto rounded color="secondary">
-                                    <Text
-                                        css={{ color: "inherit" }}
-                                        size={14}
-                                        weight="bold"
-                                        transform="uppercase"
-                                    >
-                                        Xem chi tiết
-                                    </Text>
-                                </Button>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Card.Footer>
-            </Card>
-        </Grid>
-    )
+                    </Card.Header>
+                    <Card.Body css={{ p: 0 }}>
+                        <Card.Image
+                            src={productData.image}
+                            width="100%"
+                            height="100%"
+                            objectFit="cover"
+                            alt="Card example background"
+                        />
+                    </Card.Body>
+                    <Card.Footer
+                        isBlurred
+                        css={{
+                            position: "absolute",
+                            bgBlur: "#ffffff66",
+                            borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                            bottom: 0,
+                            zIndex: 1,
+                        }}
+                    >
+                        <Row>
+                            <Col>
+                                <Text color="#000" size={14}>
+                                    {productData.name}
+                                </Text>
+                                <Text color="#000" size={14}>
+                                    {productData.price}
+                                </Text>
+                            </Col>
+                            <Col>
+                                <Row justify="flex-end">
+                                    <Button flat auto rounded color="secondary">
+                                        <Text
+                                            css={{ color: "inherit" }}
+                                            size={14}
+                                            weight="bold"
+                                            transform="uppercase"
+                                        >
+                                            Xem chi tiết
+                                        </Text>
+                                    </Button>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Card.Footer>
+                </Card>
+            </Grid>
+        )
+    }
 }
 export default ProductItem;
