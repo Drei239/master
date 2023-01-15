4076b3fec094db51bf761259418e4e99d27dcd42
DESKTOP-ORRN0CM\Toan Dang <toandang2309@gmail.com>
2023-01-15 14:28:28 +0700

    Done Login by LocalStorage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import { Button, Navbar, Text, Modal, Input } from "@nextui-org/react";
 import { useState } from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Mail } from "./Mail";
 import { Password } from "./Password";
 import mockUser from "../mocks/mockUser.json";
 
-
 const Header = () => {
     const [isVisible, setIsVisible] = useState(false);
     const dataUser = mockUser;
+    const navigate = useNavigate();
+    let loginChecked = false;
+    let isLogin = localStorage.getItem("isLogin");
+    console.log(isLogin);
 
     function openLoginModal() {
         setIsVisible(true);
@@ -23,13 +26,16 @@ const Header = () => {
         console.log(inputUsername);
         let inputPassword = document.getElementById("password")?.value;
         console.log(inputPassword);
-        let loginChecked = false;
+
         for (let i = 0; i < dataUser.length; i++) {
             if (inputUsername == dataUser[i].userName && inputPassword == dataUser[i].password) {
-                console.log("Login thành công");
-                alert("Login thành công!");
+                alert("Đăng nhập thành công!");
                 loginChecked = true;
-                sessionStorage.setItem("currentLoggedIn", inputUsername);
+                localStorage.setItem("currentLoggedIn", inputUsername);
+                localStorage.setItem("currentLoggedInId", dataUser[i].id);
+                localStorage.setItem("isLogin", true);
+                setIsVisible(false);
+                navigate("/");
                 break;
             } else {
                 console.log("Login failed");
@@ -39,7 +45,10 @@ const Header = () => {
     }
 
     function handleLogout() {
-        sessionStorage.removeItem("currentLoggedIn");
+        localStorage.removeItem("currentLoggedIn");
+        localStorage.removeItem("currentLoggedInId");
+        localStorage.removeItem("isLogin");
+        navigate("/");
     }
     return (
         <Navbar>
@@ -66,55 +75,54 @@ const Header = () => {
                     <Button size="sm">Đăng ký</Button>
                 </Navbar.Item>
                 <Navbar.Item>
-                    <Button size="sm" onClick={openLoginModal}>Đăng nhập</Button>
+                    {isLogin ? (<Button size="sm" onClick={handleLogout}>Đăng xuất</Button>)
+                        : (<Button size="sm" onClick={openLoginModal}>Đăng nhập</Button>)}
                 </Navbar.Item>
-                {/* Modal Login */}
-                <Modal blur open={isVisible} onClose={closeLoginModal}>
-                    {/* Modal Header */}
-                    <Modal.Header>
-                        <Text id="modal-title" size={18}>
-                            Welcome to
-                            <Text b size={18}>
-                                {""} Mobile Showroom
-                            </Text>
-                        </Text>
-                    </Modal.Header>
-                    {/* Modal Body */}
-                    <Modal.Body>
-                        <Input
-                            id="username"
-                            clearable
-                            bordered
-                            fullWidth
-                            color="primary"
-                            size="lg"
-                            placeholder="Email"
-                            contentLeft={<Mail fill="currentColor" />}
-                        />
-                        <Input
-                            type="password"
-                            id="password"
-                            clearable
-                            bordered
-                            fullWidth
-                            color="primary"
-                            size="lg"
-                            placeholder="Password"
-                            contentLeft={<Password fill="currentColor" />}
-                        /></Modal.Body>
-                    {/* Modal Footer */}
-                    <Modal.Footer>
-                        {/* Nút Logout dùng để test */}
-                        <Button onClick={handleLogout}>Logout</Button>
-                        <Button auto flat color="error" onClick={closeLoginModal}>
-                            Close
-                        </Button>
-                        <Button auto onClick={handleLogin}>
-                            Sign in
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
             </Navbar.Content>
+            {/* Modal Login */}
+            <Modal blur open={isVisible} onClose={closeLoginModal}>
+                {/* Modal Header */}
+                <Modal.Header>
+                    <Text id="modal-title" size={18}>
+                        Welcome to
+                        <Text b size={18}>
+                            {""} Mobile Showroom
+                        </Text>
+                    </Text>
+                </Modal.Header>
+                {/* Modal Body */}
+                <Modal.Body>
+                    <Input
+                        id="username"
+                        clearable
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="lg"
+                        placeholder="Email"
+                        contentLeft={<Mail fill="currentColor" />}
+                    />
+                    <Input
+                        type="password"
+                        id="password"
+                        clearable
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="lg"
+                        placeholder="Password"
+                        contentLeft={<Password fill="currentColor" />}
+                    /></Modal.Body>
+                {/* Modal Footer */}
+                <Modal.Footer>
+                    <Button auto flat color="error" onClick={closeLoginModal}>
+                        Close
+                    </Button>
+                    <Button auto onClick={handleLogin}>
+                        Sign in
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </Navbar>
     )
 }
