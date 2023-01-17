7777a592dd9640016644249cd734b96c4ebe616e
DESKTOP-ORRN0CM\Toan Dang <toandang2309@gmail.com>
2023-01-17 17:10:30 +0700

    FINAL UPDATE PRODUCTS PAGE

---
+++ src/components/ProductItem2.js
@@ -0,0 +1,80 @@
+import { Grid, Card, Col, Text, Row, Button } from "@nextui-org/react";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "../context/Context";
+import './ProductItem.css';
+
+const ProductItem2 = (props) => {
+    const { productData } = props;
+    const currentPrice = productData.price * (1 - productData.sale / 100);
+    const config = { style: 'currency', currency: 'VND', maximumFractionDigits: 9 }
+    const formatedPrice = new Intl.NumberFormat('vi-VN', config).format(currentPrice);
+    const { handleAddToCart } = useContext(Context);
+
+    return (
+        <Grid xs={3}>
+            <Card css={{ w: "100%", h: "400px" }}>
+                <Link className="link-detail" to={`/detail/${productData.id}`}>
+                    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+                        <Col>
+                            <Text size={14} weight="bold" transform="uppercase" color="red">
+                                New
+                            </Text>
+                            <Text size={14} weight="bold" transform="uppercase" color="red">
+                                Sale {productData.sale}%
+                            </Text>
+                        </Col>
+                    </Card.Header>
+                    <Card.Body css={{ p: 0 }} >
+                        <Card.Image
+                            src={productData.image}
+                            width="100%"
+                            height="400px"
+                            objectFit="fill"
+                            alt="Card example background"
+                        ></Card.Image>
+                    </Card.Body>
+                </Link>
+                <Card.Footer
+                    isBlurred
+                    css={{
+                        position: "absolute",
+                        bgBlur: "#ffffff66",
+                        borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                        bottom: 0,
+                        zIndex: 1,
+                    }}
+                >
+                    <Row>
+                        <Col>
+                            <Text color="#000" size={14} weight="bold">
+                                {productData.name}
+                            </Text>
+                            <Link className="link-detail" to={`/detail/${productData.id}`}>
+                                Xem chi tiết
+                            </Link>
+                        </Col>
+                        <Col>
+                            <Text color="#000" size={14} weight="bold">
+                                {formatedPrice}
+                            </Text>
+                            <Row justify="flex-end">
+                                <Button flat auto rounded color="secondary" onClick={() => { handleAddToCart(productData.id) }}>
+                                    <Text
+                                        css={{ color: "inhrederit" }}
+                                        size={14}
+                                        weight="bold"
+                                        transform="uppercase"
+                                    >
+                                        ADD TO CART
+                                    </Text>
+                                </Button>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Card.Footer>
+            </Card >
+        </Grid >
+    )
+}
+export default ProductItem2;
--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
+import { Grid, Spacer, Container, Input } from "@nextui-org/react";
+import { API_PRODUCTS_URL } from "../constants";
+import Search from "../components/Search";
+import ProductItem2 from "../components/ProductItem2";
+import mockProduct from "../mocks/mockProduct.json";
+import SlideItem from "../components/SlideItem";
+import Footer from "../components/Footer";
+import axios from "axios";
+
 const ProductsPage = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        //mockData nhà quê
+        setProducts(mockProduct);
+
+        //Fetch API = Axios
+        // axios.get(API_PRODUCTS_URL).then(({ data }) => {
+        //     console.log(data);
+        //     setProducts(data);
+        // });
+    }, []);
+
     return (
-        <div>
-            Products Page
-        </div>
+        <>
+
+            <Container>
+                <Search />
+                <Spacer y={1} />
+                <Grid.Container gap={1}>
+                    {products.map((product) => <ProductItem2 productData={product} />)}
+                    <SlideItem />
+                </Grid.Container>
+            </Container>
+            <Footer />
+        </>
     )
 }
 export default ProductsPage;
